0ef7a24588c6e4fc4667c0ea42d3bea81be6700a
=Code House - HPRTech <=ai.hprtechnology@gmail.com>
2022-12-10 19:59:39 +0530

    Important methods of PanacheRepository

--- src/main/java/org/png/resource/Resource.java
+++ src/main/java/org/png/resource/Resource.java
@@ -1,13 +1,12 @@
 package org.png.resource;
 
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.png.entity.SimCard;
 import org.png.repository.SimCardRepository;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -17,38 +16,58 @@ public class Resource {
 
     @Inject
     SimCardRepository simCardRepository;
-    
-    @GET
-    @Path("save_simcard")
+
+    @POST
     @Transactional
+    @Path("/persist_SimCard")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response saveSimCard(){
-        String[] provider = {"Jio","Airtel","VI","Aircel","BSNL"};
-
-        for(long i=0L;i<20L;i++){
-            SimCard simCard = new SimCard();
-            simCard.setNumber(8876223210L +i);
-            simCard.setProvider(provider[(int)i%provider.length]);
-            simCard.setActive(i/3L==0);
-
-            simCardRepository.persist(simCard);
-            if(simCardRepository.isPersistent(simCard)){
-                System.out.println(simCard + " saved Successfully");
-            }else{
-                System.out.println(simCard + " not saved. Please check");
-            }
+    public Response saveSimCard(@RequestBody SimCard simCard) {
+        simCardRepository.persist(simCard);
+        if (simCardRepository.isPersistent(simCard)) {
+            return Response.ok(new String("Sim Card saved successfully")).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
         }
-
-        return Response.ok(new String("Sim Card Saved Successfully")).build();
     }
 
     @GET
-    @Path("test_methods")
-    @Transactional
+    @Path("/listAll_SimCard")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response testMethods(){
+    public Response listAllSimCard() {
         List<SimCard> simCards = simCardRepository.listAll();
         return Response.ok(simCards).build();
+    }
+
+    @GET
+    @Path("/findById_SimCard/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findByIdSimCard(@PathParam("id") Long simCardId) {
+        SimCard simCard = simCardRepository.findById(simCardId);
+        return Response.ok(simCard).build();
+    }
+
+    @GET
+    @Path("/count_SimCard")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response countSimCard(@PathParam("id") Long simCardId) {
+        long count = simCardRepository.count();
+        return Response.ok(count).build();
+    }
+
+    @GET
+    @Path("/provider_list_SimCard/{provider}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response providerListSimCard(@PathParam("provider") String simCardProvider) {
+        List<SimCard> simCardList = simCardRepository.list("provider", simCardProvider);
+        return Response.ok(simCardList).build();
+    }
+
+    @GET
+    @Path("/active_list_SimCard")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response activeListSimCard() {
+        List<SimCard> simCardList = simCardRepository.list("isActive", true);
+        return Response.ok(simCardList).build();
     }
 
 }
